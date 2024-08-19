3c3c49e8bca5c9d41e637325f354090bdc7645a1
Simeon Cargile <cargileaws@gmail.com>
2024-08-19 17:51:45 -0400

    modify files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+	I need to be a cloud engineer
         </p>
         <a
           className="App-link"
